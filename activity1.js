99b66e43e4613e2b4919a8c87fdd0b450c81a5ec
sanjoli1468 <81970448+sanjoli1468@users.noreply.github.com>
2021-10-27 19:39:12 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,14 @@
+score = 0;
+
+function updatescore(){
+    score = score + 1;
+    document.getElementById("score").innerHTML = "score:" + score;
+}
+
+function savescore(){
+    localStorage.setItem("score", score);
+}
+
+function nextpage(){
+    window.location = "activity_2.html";
+}
